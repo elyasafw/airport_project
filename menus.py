c5c767179dff33a3fe04daa1a05cf521c5c5d4ae
david sh <d0504123001@gmail.com>
2026-02-04 14:50:06 +0200

    Signed-off-by: david sh <d0504123001@gmail.com>

---
+++ menus.py
@@ -0,0 +1,12 @@
+def check_user():
+    print("1.manager \n2.client \n")
+    while True:
+        user = input("manager  enter 1:  client enter 2:")
+        if user == "1":
+            return "menger"
+        elif user == "2":
+            return "client"
+        else:
+            print("please enter 1 or 2")
+
+print(check_user())
