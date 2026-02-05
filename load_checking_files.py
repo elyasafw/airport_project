9a1553246e07167315870721c79abc7d165dfaf0
aharon weber <a.w770770@gmail.com>
2026-02-05 12:45:36 +0200

    הוספה של פונקציה שמחזירה רשימת טיסות ופונקציה שמראה ללוקח תפריט והלקוח בוחר

--- load_checking_files.py
+++ load_checking_files.py
@@ -31,3 +31,40 @@ def read_write_json():
         with open(available_flights, 'r', encoding='utf-8') as f:
             airlines_list = json.load(f)
             return airlines_list
+
+
+def flight_list():
+    with open('available_flights.json', 'r', encoding='utf-8') as f:
+        airlines_data = json.load(f)
+        return airlines_data.get("available_lines", [])
+
+flight = flight_list()
+
+all_flights = flight_list()
+
+def buying_client(flights_data):
+    print("--- flight list ---")
+    for i, flight in enumerate(flights_data, 1):
+        origin = flight['origin_airport']
+        dest = flight['destination_airport']
+        print(f"{i}. {origin} -> {dest}")
+    
+    vlide = False
+    choice_num = 0 
+    
+    while not vlide:
+        choice = input("Enter your choice: ")        
+        if choice.isdigit():
+            choice_num = int(choice) 
+            
+            if 1 <= choice_num <= len(flights_data):
+                vlide = True
+            else:
+                print(f"please enter a number between 1 and {len(flights_data)}")
+        else:
+            print("please enter a number")
+            
+    return flights_data[choice_num - 1]
+
+
+
