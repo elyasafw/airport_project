38bd68a47d1dce83f9ecf179dff86a4c6eb46fcd
aharon weber <a.w770770@gmail.com>
2026-02-05 09:43:15 +0200

    אימות מנהל עובד בצורה בסיסית בקובץ main

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
 from load_checking_files import files_exiest
-from menus import menu_meneger, check_user
+from menus import check_user
 from auth import user_input, user_loading, authentication
 
 if files_exiest("credentials.csv") and files_exiest("budget.txt"):
- filename_meneger = "credentials.csv"
- filename_budget = "budget.csv"
+ file_meneger = "credentials.csv"
+ file_budget = "budget.csv"
 else:
     exit()
 
 def main():
-    print("please enter your choice:")
     if check_user() == "menger":
-        print("welcome to meneger")
-        print("please enter your username and password")
         username, password = user_input()
-        user_data = user_loading(filename_meneger)
-        print(user_data)
-        print(username, password)
+        user_data = user_loading(file_meneger)
         if authentication(username, password, user_data):
-           print("login success")
-        else:
-           print("eror")
-           exit()
-        
+           pass
+    
+           
+           
 main()
+
 
            
          
